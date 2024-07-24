22bae8228d5f7fd8050543f448ba4a4023c2896b
chavi jain <103377310+chavijain2303@users.noreply.github.com>
2024-07-24 22:20:13 +0530

    Time: 6 ms (35.32%), Space: 9.2 MB (52.15%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    int rec(int n)
-    {
-        if(n == 0)
-            return 0;
+    int rec(int n, vector<int>& memo) {
+        if (memo[n] != -1)
+            return memo[n];
 
-        if(n == 1)
-            return 1;
+        if (n == 0)
+            return memo[n] = 0;
 
-        if(n % 2 == 0)
-            return rec(n / 2);
+        if (n == 1)
+            return memo[n] = 1;
+
+        if (n % 2 == 0)
+            memo[n] = rec(n / 2, memo);
         else
-            return (1 + rec(n/2));
+            memo[n] = 1 + rec(n / 2, memo);
+        
+        return memo[n];
     }
 
-    vector<int> countBits(int n) 
-    {
-        vector<int> ans(n + 1);
+    vector<int> countBits(int n) {
+        vector<int> ans(n + 1, -1);  // Initialize memoization array with -1
         
-        for(int i = 0; i <= n; i++)
-            ans[i] = rec(i);
+        for (int i = 0; i <= n; i++)
+            ans[i] = rec(i, ans);
         
         return ans;
     }
