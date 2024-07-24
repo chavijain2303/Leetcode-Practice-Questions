b3b6e378681a341ccce24e41ffe82bc471cc4bbb
chavi jain <103377310+chavijain2303@users.noreply.github.com>
2024-07-25 00:06:22 +0530

    Time: 4 ms (36.88%), Space: 7.2 MB (43.64%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) 
+    {
+        uint32_t res = 0;
+        
+        for(int i = 0; i < 32; i++)
+        {
+            uint32_t lsb = n & 1;
+            uint32_t revLSB = lsb << (31 - i);
+            res = res | revLSB;
+            n = n >> 1;
+        }
+        return res;
+        
+    }
+};
