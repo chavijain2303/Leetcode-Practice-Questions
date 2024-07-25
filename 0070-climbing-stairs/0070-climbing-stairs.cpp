a98d9828e2da92077c2656c942efafd901dc66c0
chavi jain <103377310+chavijain2303@users.noreply.github.com>
2024-07-25 23:25:57 +0530

    Time: 0 ms (100%), Space: 7.7 MB (11.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
 
-    int memo(int n, vector<int>&dp)
+    int tabu(int n, vector<int>&dp)
     {
-        if(n == 1)
-            return 1;
-        
-        if(n == 0)
-            return 1;
+        dp[0] = dp[1] = 1;
 
-        if(dp[n] != -1)
-            return dp[n];
-
-        int left = memo(n - 1, dp);
-        int right = memo(n - 2, dp);
-
-        dp[n] = left + right;
-
+        for(int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+  
         return dp[n];
     }
 
     int climbStairs(int n) 
     {
-        //memo
-        vector<int>dp(n + 1, -1);
-        return memo(n, dp);
+        //tabulation
+        vector<int>dp(n + 1);
+        return tabu(n, dp);
     }
 };
