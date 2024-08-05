c552200d1ae4c82c38eb32eaf63bf540fb03ea3c
chavi jain <103377310+chavijain2303@users.noreply.github.com>
2024-08-05 16:18:09 +0530

    Time: 7 ms (28.63%), Space: 12.4 MB (27.09%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -4,14 +4,19 @@ public:
     {
         int n = arr.size();
 
-        for(int i = 0; i < n; i++)
-        {
-            if(arr[i] <= k)
-                k++;
+        int low = 0;
+        int high = n - 1;
 
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            int missing = arr[mid] - (mid + 1);
+
+            if(missing < k)
+                low = mid + 1;
             else
-                break;
-        } 
-        return k;       
+                high = mid - 1;
+        }    
+        return (high + k + 1);
     }
 };
