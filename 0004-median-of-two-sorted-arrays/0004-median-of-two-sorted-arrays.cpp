8bf946d28df0f11da464eadfb92a6df2967810d3
chavi jain <103377310+chavijain2303@users.noreply.github.com>
2024-08-12 23:26:28 +0530

    Time: 34 ms (17.87%), Space: 94.8 MB (47.25%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -19,10 +19,20 @@ public:
             int mid1 = low +  (high - low) / 2;
             int mid2 = left - mid1;
 
-            int l1 = (mid1 > 0) ? nums1[mid1 - 1] : INT_MIN;
-            int l2 = (mid2 > 0) ? nums2[mid2 - 1] : INT_MIN;
-            int r1 = (mid1 < n1) ? nums1[mid1] : INT_MAX;
-            int r2 = (mid2 < n2) ? nums2[mid2] : INT_MAX;
+            int l1 = INT_MIN;
+            int l2 = INT_MIN;
+            int r1 = INT_MAX;
+            int r2 = INT_MAX;
+
+            if(mid1 < n1)
+                r1 = nums1[mid1];
+            if(mid2 < n2)
+                r2 = nums2[mid2];
+
+            if(mid1 - 1 >= 0)
+                l1 = nums1[mid1 - 1];
+            if(mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
 
             if(l1 <= r2 && l2 <= r1)
             {
