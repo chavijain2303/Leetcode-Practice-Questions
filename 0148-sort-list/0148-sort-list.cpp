a0c611e6f75d0e62094508337ae81675439809b0
chavi jain <103377310+chavijain2303@users.noreply.github.com>
2024-08-27 23:54:42 +0530

    Time: 161 ms (14.42%), Space: 74.9 MB (24.35%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,32 +10,63 @@
  */
 class Solution {
 public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) 
+    {
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+
+        while(list1 != nullptr && list2 != nullptr)
+        {
+            if(list1 ->val < list2 -> val)
+            {
+                temp -> next = list1;
+                temp = list1;
+                list1 = list1 -> next;
+            }
+            else
+            {
+                temp -> next = list2;
+                temp = list2;
+                list2 = list2 -> next;
+            }
+        }
+        if(list1)
+            temp -> next = list1;
+        else
+            temp -> next = list2;
+
+        return dummyNode -> next;
+    }
+
+    ListNode* findMiddle(ListNode* head)
+    {
+        ListNode* slow = head;
+        ListNode* fast = head -> next;
+
+        while(fast != nullptr && fast -> next != nullptr)
+        {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+
+        return slow;
+    }
     ListNode* sortList(ListNode* head) 
     {
         if(head == nullptr || head -> next == nullptr)
             return head;
 
-        vector<int> ls;
-        int i;
+        ListNode* mid = findMiddle(head);
 
-        ListNode* temp = head;
+        ListNode* leftHead = head;
+        ListNode* rightHead = mid -> next;
 
-        while(temp != nullptr)
-        {
-            ls.push_back(temp -> val);
-            i++;
-            temp = temp -> next;
-        }
+        mid -> next = nullptr;
 
-        sort(ls.begin(), ls.end());
-        temp = head;
+        leftHead = sortList(leftHead);
+        rightHead = sortList(rightHead);
 
-        for(int j = 0; j < ls.size(); j++)
-        {
-            temp -> val = ls[j];
-            temp = temp -> next;        
-        }
-
-        return head;
+        return mergeTwoLists(leftHead, rightHead); 
+        
     }
 };
